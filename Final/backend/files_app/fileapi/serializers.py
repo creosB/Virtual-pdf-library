d1d92d64db3a8e5d0d10ec94fa99b313353b6c41
creosB <baris0631@hotmail.com>
2021-12-23 04:52:23 +0300

    fix

--- Final/backend/files_app/fileapi/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from filetest import models
-
-class FileSerializer(serializers.ModelSerializer):
-    class Meta:
-        fields = ('id', 'name', 'size', 'data')
-        model = models.Files
