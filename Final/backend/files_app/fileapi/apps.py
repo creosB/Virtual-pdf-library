07e8388cd55d5460adf952e3ca273d9747d4ad83
creosB <baris0631@hotmail.com>
2021-12-23 06:10:23 +0300

    fix

---
+++ Final/backend/files_app/fileapi/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FileapiConfig(AppConfig):
+    name = 'fileapi'
---
+++ Final/backend/files_app/filetest/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from filetest.models import Files
+
+# Register your models here.
+
+admin.site.register(Files)
